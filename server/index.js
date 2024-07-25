c1116735d056b5d1cbe737d75c7994a16007a592
Nanmaran M <maaran1314@gmail.com>
2024-07-25 12:39:28 +0530

    Registration page updating

--- server/index.js
+++ server/index.js
@@ -35,11 +35,11 @@ let limiter = rateLimit({
 
 // Middlewares
 app.use(cors({
-    origin: 'https://authentication-deploy-client.vercel.app', // Replace with your client URL
+    origin: 'https://authentication-deploy-client.vercel.app/', // Replace with your client URL
     methods: ['GET', 'POST', 'PUT', 'DELETE'], // Adjust methods as needed
     allowedHeaders: ['Content-Type', 'Authorization'] // Adjust headers as needed
 }));
-app.use(express.json({limit:'20kb'})); // Middleware for JSON 
+app.use(express.json({limit:'50kb'})); // Middleware for JSON 
 app.use(helmet()); //Adding Security Headers
 app.use(sanitize()); //Sanitize NoSQL injection Attacks
 app.use(xssSanitize()); //Sanitize XSS injection Attacks
